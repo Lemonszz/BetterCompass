bf99ebbc274041b399b6c05f1702a1f14c02bd89
Jason Spangler <jason.spangler@descendentstudios.com>
2018-06-23 14:13:40 -0500

    Option for separate homing compass with code, art, and data

--- src/main/java/party/lemons/bettercompass/config/ModConfig.java
+++ src/main/java/party/lemons/bettercompass/config/ModConfig.java
@@ -1,6 +1,8 @@
 package party.lemons.bettercompass.config;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Name;
+import net.minecraftforge.common.config.Config.RequiresMcRestart;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,6 +20,11 @@ public class ModConfig
 	public static boolean showLocationInfoText = false;
 	public static boolean allowCompassInAllDimensions = false;
 	public static CompassSetting compassActivateType = CompassSetting.RIGHT_CLICK;
+
+	@Name("Use Homing Compass Instead")
+	@Config.Comment("Create new Homing Compass recipe instead of overriding standard Compass recipe.")
+	@RequiresMcRestart
+	public static boolean useHomingCompassInstead = false;
 
 	@SubscribeEvent
 	public static void onConfigChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
