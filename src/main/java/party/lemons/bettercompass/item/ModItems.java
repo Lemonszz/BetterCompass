988b25ddfa75147f63bc63ec254c62b04239bb6f
Sam <lemonverse@gmail.com>
2018-06-24 12:06:33 +1200

    up version

--- src/main/java/party/lemons/bettercompass/item/ModItems.java
+++ src/main/java/party/lemons/bettercompass/item/ModItems.java
@@ -1,6 +1,7 @@
 package party.lemons.bettercompass.item;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
@@ -10,6 +11,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import party.lemons.bettercompass.config.ModConfig;
 
 /**
  * Created by Sam on 3/02/2018.
